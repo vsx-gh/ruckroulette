1bdf06c0f4f542f483d1e7429d16698465337484
Jeff VanSickle <jvansickle@sickletech.com>
2019-11-19 23:03:33 -0600

    Clean up routing, add nav bar layout

--- app.py
+++ app.py
@@ -14,23 +14,27 @@ app = Flask(__name__)
 app.debug = True
 
 # PT moves and body area worked (subject to change/interpretation)
-pt_moves = [{'name': 'Ruck Squat', 'area': 'leg'}, {'name': 'Lunges', 'area': 'leg'}, {'name': 'Ruck Overhead Squat', 'area': 'both'},
+pt_moves = [{'name': 'Ruck Squat', 'area': 'leg'}, {'name': 'Lunges', 'area': 'leg'}, {'name': 'Ruck Overhead Squat', 'area': 'arm/leg'},
         {'name': 'Push-up', 'area': 'arm'}, {'name': 'Ruck High Pull', 'area': 'arm'}, {'name': 'Overhead Shoulder Press', 'area': 'arm'},
-        {'name': 'Bear Crawl', 'area': 'both'}, {'name': 'Suitcase Carry', 'area': 'arm'}, {'name': 'Russian Twist', 'area': 'abs'},
-        {'name': 'Plank Pulls', 'area': 'abs'}, {'name': 'Ruck Swings', 'area': 'arm'}, {'name': 'Thruster', 'area': 'both'}, 
-        {'name': 'Deadlift', 'area': 'both'}, {'name': 'Flutter Kicks', 'area': 'abs'}]
+        {'name': 'Bear Crawl', 'area': 'arm/leg'}, {'name': 'Suitcase Carry', 'area': 'arm'}, {'name': 'Russian Twist', 'area': 'abs'},
+        {'name': 'Plank Pulls', 'area': 'abs'}, {'name': 'Ruck Swings', 'area': 'arm'}, {'name': 'Thruster', 'area': 'arm/leg'}, 
+        {'name': 'Deadlift', 'area': 'arm/leg'}, {'name': 'Flutter Kicks', 'area': 'abs'}]
 
 # Body area selection in drop-down menu
-body_area = ['Leg', 'Arm', 'Both', 'Abs', 'Any']
+body_area = ['Leg', 'Arm', 'Arm/Leg', 'Abs', 'Any']
+moves_max = 5
 
 @app.route('/', methods=['GET', 'POST'])
-def dropdown():
-    return render_template('index.html', body_areas=body_area)
+def display_dropdown():
+    print('Endpoint: {}'.format(request.endpoint))
+    return render_template('index.html', body_areas=body_area, num_moves=moves_max, page='index')
 
 @app.route('/results.html', methods=['GET', 'POST'])
-def get_moves():
+def get_workout():
     if request.method == 'POST':
+        moves_count = int(request.form['num_moves'])
         workout_type = request.form['body_area']
+        print('Endpoint: {}'.format(request.endpoint))
 
     # Select any area, else get moves only from the area chosen by user
     if workout_type == 'Any':
@@ -41,8 +45,8 @@ def get_moves():
     do_these_moves = []     # Hold moves that match body area selected
     curr_move = 0           # Move counter
 
-    # Cap number of moves at 3; follows GORUCK training plans (mostly)
-    while curr_move < 3:
+    # Cap number of moves at moves_max 
+    while curr_move < moves_count: 
         if curr_move > len(workout_list) - 1:     # Don't get more moves than available
             break
 
@@ -56,7 +60,12 @@ def get_moves():
         else:
             continue
 
-    return render_template('results.html', body_areas=body_area, workout=do_these_moves)
+    return render_template('results.html', body_areas=body_area, workout=do_these_moves, num_moves=moves_max, page='results')
+
+@app.route('/create.html', methods=['GET', 'POST'])
+def create_workout():
+    print('Endpoint: {}'.format(request.endpoint))
+    return render_template('create.html', page='create')
 
 if __name__ == "__main__":
     app.run()
